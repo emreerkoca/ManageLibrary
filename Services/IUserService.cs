1727785dce6be55f3cf59b29fb281a006d660273
Emre Erkoca <emreerkoca@outlook.com>
2019-10-29 01:29:43 +0300

    Web api and client app created

---
+++ Services/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using ManageLibrary.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManageLibrary.Services
+{
+    interface IUserService
+    {
+        User Authenticate(string username, string password);
+    }
+}
---
+++ Services/UserService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ManageLibrary.Entities;
+using Microsoft.Extensions.Configuration;
+
+namespace ManageLibrary.Services
+{
+    public class UserService : IUserService
+    {
+        public UserService(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        private List<User> _users = new List<User>
+        {
+            new User { Id = 1, FirstName = "Carl", LastName = "Johnson", Username = "test", Password = "test" }
+        };
+
+        public User Authenticate(string username, string password)
+        {
+            
+            throw new NotImplementedException();
+        }
+    }
+}
